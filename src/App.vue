fc0bd6977015879593ba2012ebe13a9ef2ad7a5a
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2024-04-16 14:17:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <img
+      src="https://img.freepik.com/free-vector/flat-construction-template_23-2147735518.jpg?t=st=1713273302~exp=1713276902~hmac=8b528702145849ed86624e95d4788a3c31cba587687b86c2cc1f9cd17a4f014a&w=740"
+      alt=""
+    />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
