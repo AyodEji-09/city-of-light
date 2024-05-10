396504c227d2ab62792b71b838579aca9e049033
chris <taiwoakinfenwa2019@gmail.com>
2024-05-10 17:40:06 +0100

    about&servicePage

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -1,7 +1,162 @@
 <template>
-    <div class="container-fluid p-0 mb-5">
-      <SubHeader page="Service"/>
-     Service page
+    <div class="container-fluid p-0 mb-5">  
+       <SubHeader page="Service"/> 
+        <div class="d-flex  justify-content-center gap-5 flex-column align-items-center">
+           <div class="d-flex container-sm justify-content-center flex-column gap-2 align-items-center"> 
+              <h3 class="text-primary">WORSHIP & PRAISE</h3>  
+              <p class="text-center"> COL’s Friday gatherings will include praise  
+                & worship as directed by the Spirit of God and of all Christian genre. 
+              </p> 
+              <div class="row px-5"> 
+                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                  />
+
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Wintry Mountain Landscape"
+                  />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Mountains in the Clouds"
+                  />
+
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                  />
+                </div>
+              
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Waves at Sea"
+                  />
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Yosemite National Park"
+                    />
+                 </div>
+            </div>   
+            </div>
+            <div class="d-flex container justify-content-center flex-column gap-2 align-items-center">
+              <h3 class="text-primary">PRAYERS</h3>  
+              <div class="d-flex flex-lg-row justify-content-center align-content-center collapse navbar-collapse">
+              <div class="  wow slideInLeft " data-wow-delay="0.1s">
+                <p class="px-5">COL’s will engage prayers of all kinds as enabled by 
+                  the Holy Spirit. We will engage the power of intercession,
+                  standing with men and women weekly on their needs and desires.
+                  City of Lights will be building men and women to develop an independent prayer culture.
+                </p>  
+              </div>
+              <div class="row ">
+<!--                 <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                  />
+
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Wintry Mountain Landscape"
+                  />
+                </div> -->
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Mountains in the Clouds"
+                  />
+
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                  />
+                </div>
+              
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Waves at Sea"
+                  />
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Yosemite National Park"
+                    />
+                 </div>
+            </div>   
+            </div>
+          </div>
+            <div class="d-flex container justify-content-center flex-column gap-2 align-items-center">
+              <h3 class="text-primary">WORD</h3>  
+              <p class=" text-center">COL’s services will include Spirit inspired sermons,
+                 exhortations and bible study small groups. Bible studies
+                  will be our most used tool in sharing the word amongst
+                   brethren. These Bible studies will be mostly done online 
+                   and in person small-groups.
+              </p>
+              <div class="row px-5">
+                <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                  />
+
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Wintry Mountain Landscape"
+                  />
+                </div>
+
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Mountains in the Clouds"
+                  />
+
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Boat on Calm Water"
+                  />
+                </div>
+              
+                <div class="col-lg-4 mb-4 mb-lg-0">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Waves at Sea"
+                  />
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
+                    class="w-100 shadow-1-strong rounded mb-4"
+                    alt="Yosemite National Park"
+                    />
+                 </div>
+            </div>     
+            </div>
+            
+       </div>
     </div>
   </template>
   <script setup>
